c74bbe5f01053032e4b287c095c077a28567545d
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2024-05-13 18:59:42 -0300

    Implemented Interfaces and Classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace MicroRabbit.Domain.Core
+{
+    public abstract class Event
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        protected Event()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
