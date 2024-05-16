7bb8a9114d532298303ea30a0f9498eebb7f0f0e
Cleber do Nascimento <cleber.nsbrito@gmail.com>
2024-05-16 18:09:38 -0300

    Initial Commit of Infrastructure Dependency Project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependecyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependecyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
